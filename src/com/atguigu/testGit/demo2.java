f7cffdbf0dfad05b589dd618d3b389f886b0cd7e
limber <limber@qq.com>
2022-11-28 15:22:15 +0800

    first

--- src/com/atguigu/testGit/demo2.java
+++ src/com/atguigu/testGit/demo2.java
@@ -1,7 +1,7 @@
 package com.atguigu.testGit;
 
-public class demo1 {
+public class demo2 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world!!!");
     }
 }
