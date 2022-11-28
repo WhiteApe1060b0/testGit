f8f37a4e5a8fe28aa77452d13df093e0bb61783d
limber <limber@qq.com>
2022-11-28 15:25:20 +0800

    third

--- src/com/atguigu/testGit/demo2.java
+++ src/com/atguigu/testGit/demo2.java
@@ -4,5 +4,9 @@ public class demo2 {
     public static void main(String[] args) {
         System.out.println("hello world!!!");
         System.out.println("hello world!!!");
+        System.out.println("hello world!!!");
+        for(int i=0; i<10; i++) {
+            System.out.println("ÄãºÃÊÀ½ç£¡");
+        }
     }
 }
